aa22321103a42de340fff660f61fb832f3b0d153
Iusti T <iustilaurentiu@yahoo.ro>
2023-09-30 21:00:06 +0100

    Finish 'frequencies' function

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,11 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    while len(items) > 0:
+        current_item = items.pop(0)
+        if current_item not in frequencies:
+            frequencies[current_item] = 1
+        else:
+            frequencies[current_item] += 1
+
     return frequencies
